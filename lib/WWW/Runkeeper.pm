30b150d0370d2dac091a092d180e49958e10cd79
Jonathan "Duke" Leto <jonathan@leto.net>
2012-12-04 16:36:23 -0800

    Ask Moose for immutability and add a basic synopsis

--- lib/WWW/Runkeeper.pm
+++ lib/WWW/Runkeeper.pm
@@ -5,4 +5,13 @@ use namespace::autoclean;
 use LWP::UserAgent;
 use JSON::Any;
 
-1;
+__PACKAGE__->meta->make_immutable;
+
+__END__
+
+=head1 SYNOPSIS
+
+    use WWW::Runkeeper;
+    my $rk = WWW::Runkeeper->new;
+
+=cut
